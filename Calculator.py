6f27fec6699d2c26a773886239ae61bfb5d76e19
Ghrislain <ghrislain@Ghrislains-MacBook-Pro.local>
2020-12-10 15:33:22 -0700

    TAB != SPACE

--- Calculator.py
+++ Calculator.py
@@ -1,33 +1,51 @@
-print('''
-Caluclator V0.1
-Which would you like to do?
-+ for Addition
-- for Subtraction
-* for Multiplication
-/ for Division''')
-operation = input(': ')
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
 
-number_1 = int(input('First Number: '))
-number_2 = int(input('Second Number: '))
+    number_1 = int(input('First Number: '))
+    number_2 = int(input('Second Number: '))
 
-#print(num_1)
-#print(num_2)
+    if operation == '+':
+   	    print('{} + {} = '.format(number_1, number_2))
+   	    print(number_1 + number_2)
 
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
 
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    elif operation == '/':
+	    print('{} / {} = '.format(number_1, number_2))
+	    print(number_1 / number_2)
 
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+    else:
+        print('Invalid operator used.')
 
-else:
-    print('You have not typed a valid operator, please run the program again.')
+    repeat()
+
+def repeat():
+
+	calc_repeat = input('''
+	Another calculation? Y or N
+	''')
+
+	# Accept 'y' or 'Y' by adding str.upper()
+	if calc_repeat.upper() == 'Y':
+		calculate()
+
+	    # Accept 'n' or 'N' by adding str.upper()
+	elif calc_repeat.upper() == 'N':
+	    print('Exiting.')
+
+	else:
+		again()
+
+calculate()
